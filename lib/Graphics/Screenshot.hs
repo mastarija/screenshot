f4a8e10d6d5b551d3408818a30d1e644a4ae2f42
Maštarija <51950685+mastarija@users.noreply.github.com>
2021-01-05 22:56:21 +0100

    prepared the screenshot interface / types

--- lib/Graphics/Screenshot.hs
+++ lib/Graphics/Screenshot.hs
@@ -2,35 +2,14 @@
 --
 module Graphics.ScreenShot where
 --
-import GHC.Generics ( Generic )
-import Data.ByteString ( ByteString )
---
 
-{-
-  A @record@ representing coordinates and dimensions of real screen on
-  a virtual screen.
--}
 data Screen = Screen
-  { x :: Int -- ^ @x@ coordinate
-  , y :: Int -- ^ @y@ coordinate
-  , w :: Int -- ^ width
-  , h :: Int -- ^ height
-  } deriving ( Eq, Show, Generic )
+  { x :: !Int
+  , y :: !Int
+  , w :: !Int
+  , h :: !Int
+  }
 
-{-
-  A @newtype@ wrapper around 'ByteString' used to represent screenshot
-  data in @BMP@ format. This can be further processed into more
-  appropriate image formats.
--}
-newtype Bitmap = Bitmap
-  { unBitmap :: ByteString -- ^ @BMP@ data
-  } deriving ( Eq, Show, Generic )
-
-{-
-  Screen shot interface defined as a simple record.
--}
 data ScreenShot = ScreenShot
-  { screenEnum :: IO [Screen]                 -- ^ list all screens
-  , screenShot :: Screen -> IO Bitmap         -- ^ take a screenshot
-  , screenSave :: FilePath -> Bitmap -> IO () -- ^ save bitmap to disk
+  {
   }
